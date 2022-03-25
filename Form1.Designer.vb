d60fee340de7273e37f985a54abb954576068b1a
Matteo Barzaghi <matteo.barzaghi91@gmail.com>
2022-03-25 11:10:54 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.btnVariables = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(12, 194)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(492, 208)
+        Me.btnGo.TabIndex = 0
+        Me.btnGo.Text = "Go!"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'btnVariables
+        '
+        Me.btnVariables.Location = New System.Drawing.Point(12, 12)
+        Me.btnVariables.Name = "btnVariables"
+        Me.btnVariables.Size = New System.Drawing.Size(492, 176)
+        Me.btnVariables.TabIndex = 1
+        Me.btnVariables.Text = "Variables"
+        Me.btnVariables.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(516, 413)
+        Me.Controls.Add(Me.btnVariables)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGo As Button
+    Friend WithEvents btnVariables As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        ' two ways to show messages
+        MsgBox("Hello VB .NET")
+        MsgBox("This is the first type of Message box")
+
+        ' object oriented approach to show message
+        MessageBox.Show("This is the second type of Message box")
+
+    End Sub
+
+    Private Sub btnVariables_Click(sender As Object, e As EventArgs) Handles btnVariables.Click
+
+        ' a variable is a location in the computer memory where is stored an information
+
+        ' declaring a variable
+        Dim shortForDimension As String = "This is stored somewhere in the memory"
+        MessageBox.Show(shortForDimension)
+
+        Dim name As String = "Matteo"
+        Dim surname As String = "Barzaghi"
+
+        MessageBox.Show("Hello " & name & " " & surname & ", this is an example of string concatenation")
+
+    End Sub
+End Class
